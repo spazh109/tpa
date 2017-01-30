2082d95936545129e46125808b8a52cd4145fec8
spazh109 <spazh109@gmail.com>
2017-01-30 10:35:49 +0300

    rel 20170129

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes } from '@angular/router';
+import { RouterModule} from "@angular/router";
+
+import { ModalModule }  from 'ng2-modal';
 
 import { AppComponent } from './app.component';
 
+import { SpinnerService} from './spinner/spinner.service';
+import { SpinnerComponent} from './spinner/spinner.component';
+
+import { AuthService }    from './auth/auth.service';
+import { SaveGuard } from "./auth/save-guard.service";
+
+//----------------------------------------------------------------------------------------------
+
+import { DummyComponent } from './dummy/dummy.component';
+import { ChatVersionModule,ChatVersionList,ChatVersionDetail} from "./chat-version/chat-version.module";
+
+const ROUTES: Routes = [
+    { path: '', redirectTo: 'app', pathMatch: 'full' },
+    { path: 'app',  component: DummyComponent},
+    { path: 'chat_version', component: ChatVersionList },
+    { path: 'chat_version/:id', component: ChatVersionDetail, canDeactivate: [SaveGuard]},
+];
+
+//----------------------------------------------------------------------------------------------
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
+    DummyComponent,  
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ModalModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+
+    ChatVersionModule,
   ],
-  providers: [],
+  providers: [SpinnerService, AuthService, SaveGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+
+  API_URL: "http://therapybot-api.vp-software.com",
+  API_REMEMBER_ME: false,
+
+  API_NOTIFICATION: false, // true;
+  API_NOTIFICATION_INTERVAL: 30,    // sec
+
 };
